718a413cdb99bb479f5af1164d496df9ede2454a
Nadezhda Petrova <71227223+nkpetrova@users.noreply.github.com>
2022-07-14 12:58:20 +0300

    Add files via upload

---
+++ Models/Tur_Firma.cs
@@ -0,0 +1,33 @@
+﻿namespace BlogsApp.Models
+{
+    public class TurFirma
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Address { get; private set; }
+        public int Commission { get; private set; }
+
+        public TurFirma( int id, string name, string address, int commission )
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Commission = commission;
+        }
+
+        public void UpdateName(string newName)
+        {
+            Name = newName;
+        }
+
+        public void UpdateAddress(string newAddress)
+        {
+            Address = newAddress;
+        }
+
+        public void UpdateCommission(int newCommission)
+        {
+            Commission = newCommission;
+        }
+    }
+}
---
+++ Models/Voucher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace BlogsApp.Models
+{
+    public class Voucher
+    {
+        public int Id { get; private set; }
+        public string Sanatorium { get; private set; }
+        public string Address { get; private set; }
+        public int Price { get; private set; }
+        public int Quantity { get; private set; }
+
+        public Voucher(int id, string sanatorium, string address, int price, int quantity)
+        {
+            Id = id;
+            Sanatorium = sanatorium;
+            Address = address;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
