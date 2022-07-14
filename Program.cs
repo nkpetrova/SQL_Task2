f245abf2375d9225f86e1ddbe450473736604793
Nadezhda Petrova <71227223+nkpetrova@users.noreply.github.com>
2022-07-14 13:03:29 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
-﻿using BlogsApp.Models;
-using BlogsApp.Repositories;
+﻿using SQLTASK2.Models;
+using SQLTASK2.Repositories;
 
 const string connectionString = @"Server=localhost;Database=BOOKING;User Id=sa;Password=reallyStrongPwd123;MultipleActiveResultSets=true";
 
@@ -13,24 +13,30 @@ while ( true )
     Console.WriteLine( "Введите команду:" );
     string command = Console.ReadLine();
 
-    if ( command == "get-bookings" )
+    if (command == "get-bookings")
     {
         IReadOnlyList<Booking> bookings = bookingRepository.GetAll();
-        if (bookings.Count == 0 )
+        if (bookings.Count == 0)
         {
-            Console.WriteLine( "Заказы не найдены!" );
+            Console.WriteLine("Заказы не найдены!");
             continue;
         }
 
-        foreach ( Booking booking in bookings)
+        foreach (Booking booking in bookings)
         {
-            Console.WriteLine( $"Id: {booking.Id}, Date: {booking.Date},  Turfirma_id: {booking.Turfirma_id}, Voucher_id: {booking.Voucher_id}, Quantity: {booking.Quantity}, Price: {booking.Price}" );
+            Console.WriteLine($"Id: {booking.Id}, Date: {booking.Date},  Turfirma_id: {booking.Turfirma_id}, Voucher_id: {booking.Voucher_id}, Quantity: {booking.Quantity}, Price: {booking.Price}");
         }
     }
     else if (command == "get-by-sanatorium")
     {
         Console.WriteLine("Введите название санатория:");
         string sanatorium = Console.ReadLine();
+        if (string.IsNullOrEmpty(sanatorium))
+        {
+            Console.WriteLine("Неккоректный ввод!");
+            continue;
+        }
+
         Voucher voucher = voucherRepository.GetByNameOfSanatorium(sanatorium);
         if (voucher == null)
         {
@@ -45,7 +51,14 @@ while ( true )
     else if (command == "update-turfirma")
     {
         Console.WriteLine("Введите Id:");
-        int id = int.Parse(Console.ReadLine());
+        string strid = Console.ReadLine();
+        int id;
+        if (!int.TryParse(strid, out id))
+        {
+            Console.WriteLine("Неккоректный ввод!");
+            continue;
+        }
+
         TurFirma turfirma = turfirmaRepository.GetById(id);
         if (turfirma == null)
         {
@@ -55,15 +68,31 @@ while ( true )
 
         Console.WriteLine("Введите новое значение названия тур. фирмы:");
         string newName = Console.ReadLine();
+        if (string.IsNullOrEmpty(newName))
+        {
+            Console.WriteLine("Неккоректное название тур. фирмы!");
+            continue;
+        }
         turfirma.UpdateName(newName);
 
         Console.WriteLine("Введите новое значение адреса тур. фирмы:");
         string newAddress = Console.ReadLine();
+        if (string.IsNullOrEmpty(newAddress))
+        {
+            Console.WriteLine("Неккоректный адрес тур. фирмы!");
+            continue;
+        }
         turfirma.UpdateAddress(newAddress);
 
         Console.WriteLine("Введите новое значение комиссии:");
-        int newCommission = int.Parse(Console.ReadLine());
-        turfirma.UpdateCommission(newCommission);
+        string newCommission = Console.ReadLine();
+        int newCom;
+        if (!int.TryParse(newCommission, out newCom))
+        {
+            Console.WriteLine("Неккоректный ввод!");
+            continue;
+        }
+        turfirma.UpdateCommission(newCom);
 
         turfirmaRepository.UpdateTurFirma(turfirma);
         Console.WriteLine("Информация о тур. фирме обновлена!");
@@ -71,7 +100,14 @@ while ( true )
     else if (command == "delete-turfirma-by-id")
     {
         Console.WriteLine("Введите id:");
-        int id = int.Parse(Console.ReadLine());
+        string strid = Console.ReadLine();
+        int id;
+        if (!int.TryParse(strid, out id))
+        {
+            Console.WriteLine("Неккоректный ввод!");
+            continue;
+        }
+
         TurFirma turfirma = turfirmaRepository.GetById(id);
         if (turfirma == null)
         {
@@ -83,20 +119,21 @@ while ( true )
             Console.WriteLine("Тур. фирма удалена!");
         }
     }
-    //else if (command == "group-by-vouchers")
-    //{
-    //    Console.WriteLine("Группировка по санаториям:");
+    else if (command == "group-by-date")
+    {
 
-    //    Voucher voucher = voucherRepository.GroupByAddress();
-    //    if (voucher == null)
-    //    {
-    //        Console.WriteLine("Путевка не найдена!");
-    //    }
-    //    else
-    //    {
-    //        Console.WriteLine($"Id: {voucher.Id}, Name: {voucher.Sanatorium}, Address: {voucher.Address}, Price: {voucher.Price}, Quantity: {voucher.Quantity}");
-    //    }
-    //}
+        IReadOnlyList<Tuple<Booking, int>> bookings = bookingRepository.GroupByDate();
+        if (bookings.Count == 0)
+        {
+            Console.WriteLine("Заказ не найден!");
+            continue;
+        }
+
+        foreach (var booking in bookings)
+        {
+            Console.WriteLine($"Date: {booking.Item1.Date}, TotalPrice: {booking.Item2}");
+        }
+    }
     else if (command == "help")
     {
         PrintCommands();
@@ -107,7 +144,7 @@ while ( true )
     }
     else
     {
-        Console.WriteLine( "Неправильно введенная команда" );
+        Console.WriteLine("Неправильно введенная команда");
     }
 }
 
@@ -116,6 +153,7 @@ void PrintCommands()
     Console.WriteLine( "Доступные команды:" );
     Console.WriteLine( "get-bookings - Получить список всех заказов" );
     Console.WriteLine( "get-by-sanatorium - Получить информацию о путевке по названию санатория" );
+    Console.WriteLine( "group-by-date - Вывести общую сумму заказов по датам" );
     Console.WriteLine( "update-turfirma - Обновить  информацию о тур. фирме" );
     Console.WriteLine( "delete-turfirma-by-id - Удалить тур. фирму по id");
     Console.WriteLine( "help - Список команд" );
